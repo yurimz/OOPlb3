f2ad149755a9191129be3c2b1ad90a559cb6fe71
yurimz <0xC0DEmz@gmail.com>
2022-10-31 21:53:34 +0300

    Remove `cmake-build-debug` binaries
    Looks like build artefacts with binary blob, therefore:
     - not needed
     - leaks private information about build environment
     - negatively affects git performance due binary files storage

--- OOPLB3/cmake-build-debug/CMakeFiles/untitled2.dir/Log/StatLog.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/untitled2.dir/Log/StatLog.cpp.o: \
- /home/akimfeopentov/Рабочий\ стол/OOPLB3/Log/StatLog.cpp \
- /usr/include/stdc-predef.h \
- /home/akimfeopentov/Рабочий\ стол/OOPLB3/Log/StatLog.h
